b84cf22bab4018c84ac23f71685702c26d5f5939
mboersch <46405965+mboersch@users.noreply.github.com>
2019-06-19 20:00:09 +0200

    ircd-go: ssl server

--- ircd-go/ircd.go
+++ ircd-go/ircd.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    //"crypto/tls"
+    "crypto/tls"
     "strings"
     "fmt"
     "flag"
@@ -9,12 +9,15 @@ import (
     "os"
     "os/exec"
     "io/ioutil"
+    "io"
     "bytes"
+    "net"
+    "strconv"
 )
 type config struct {
     //config
     address  string
-    port     uint
+    port     string
     certfile string
     doSelfsigned bool
 }
@@ -30,8 +33,9 @@ type ircError struct {
 func (err ircError) Error() string {
     return fmt.Sprintf("IRC-ERROR: %d: %s", err.code, err.msg)
 }
-const cmd_selfsign string = "openssl req -x509 -newkey rsa:4096 -keyout - -out - -days 3650 -nodes -subj /C=DE/ST=BW/L=BW/O=Faul/OU=Org/CN=%s"
 func makeCert(ircd *ircd) error {
+    const cmd_selfsign string = "openssl req -x509 -newkey rsa:4096 -keyout -" +
+        " -out - -days 3650 -nodes -subj /C=DE/ST=BW/L=BW/O=Faul/OU=Org/CN=%s"
     myaddr := ircd.config.address
     filename := ircd.config.certfile
     if strings.Index(myaddr, " ")  != -1 {
@@ -59,12 +63,13 @@ func (self *ircd) usage(msg string ) {
     os.Stderr.WriteString(fmt.Sprintf("ERROR: %s\n", msg))
 }
 func (self *ircd) parseArgs() error {
-    flag.UintVar(&self.config.port, "port", 6697, "specify the port number to listen on")
+    flag.StringVar(&self.config.port, "port", "6697", "specify the port number to listen on")
     flag.StringVar(&self.config.address, "address", "localhost", "specify the internet address to listen on")
     flag.StringVar(&self.config.certfile, "certfile", "", "specify the ssl PEM certificate to use for TLS server")
     flag.BoolVar(&self.config.doSelfsigned, "selfsigned", false, "use openssl commands to generate a selfsigned certificate use (development use only)")
 	flag.Parse()
-    if self.config.port > uint(^uint16(0)) {
+    tmp, err := strconv.Atoi(self.config.port)
+    if err != nil  || tmp > int(^uint16(0)) {
         self.usage("the specified port is invalid")
         return ircError{-1, "invalid port"}
     }
@@ -72,7 +77,7 @@ func (self *ircd) parseArgs() error {
         self.usage("please specify a certificate file. Or use '-selfsigned' to generate one")
         return  ircError{-2, "invalid cert file"}
     }
-     _, err := os.Stat(self.config.certfile)
+    _ , err = os.Stat(self.config.certfile)
     if os.IsNotExist(err) {
         if self.config.doSelfsigned {
             if err := makeCert(self); err != nil {
@@ -89,10 +94,40 @@ func (self *ircd) parseArgs() error {
 func New() *ircd {
     rv := new(ircd)
     rv.log  = log.New(os.Stdout, "ircd ", log.LstdFlags)
-    rv.parseArgs()
+    if err := rv.parseArgs(); err != nil {
+        rv.log.Printf("ERROR: %s", err)
+        return nil
+    }
     return rv
 }
 func (self *ircd) run() {
+    crt, err := tls.LoadX509KeyPair(self.config.certfile, self.config.certfile)
+    if err != nil {
+        self.log.Fatalf("Cannot load cert/key pair: %s", err)
+        return
+    }
+    cfg := &tls.Config{Certificates: []tls.Certificate{crt}}
+    addr := fmt.Sprintf("%s:%s", self.config.address, self.config.port)
+    self.log.Printf("Listening on %s", addr)
+    l, err := net.Listen("tcp", addr)
+    if err != nil {
+        self.log.Fatalf("Cannot create listening socket on  %s", addr)
+        return
+    }
+    defer l.Close()
+    for {
+        cl, err :=  l.Accept()
+        if err != nil {
+            self.log.Printf("ERROR: cannot accept client %v", err)
+            continue
+        }
+        client := tls.Server(cl, cfg)
+        go func(c net.Conn) {
+            io.Copy(os.Stdout, c)
+            fmt.Println()
+            c.Close()
+        }(client)
+    }
 }
 func main() {
     server :=  New()
